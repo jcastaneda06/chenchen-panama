4e0e35fd932c533dc436d7495936e6dd0106fadf
Jesus Castaneda <jcs.98126@gmail.com>
2025-05-10 12:53:30 -0500

    Add transparency changes (#9)

--- src/helpers/formatSalary.ts
+++ src/helpers/formatSalary.ts
@@ -1,15 +1,16 @@
-export const formatSalary = (salary: number, multiplier: "d" | "m" | "y") => {
-  switch (multiplier) {
-    case "d":
-      salary = salary / 30;
-      break;
-    case "y":
-      salary = salary * 12;
-      break;
-    default:
-      salary;
-      break;
-  }
+export const formatSalary = (salary: number, multiplier?: "d" | "m" | "y") => {
+  if (multiplier)
+    switch (multiplier) {
+      case "d":
+        salary = salary / 30;
+        break;
+      case "y":
+        salary = salary * 12;
+        break;
+      default:
+        salary;
+        break;
+    }
 
   const formatted = new Intl.NumberFormat("en-US", {
     style: "currency",
